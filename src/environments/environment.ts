197355a412a009f37b46f0a6d7a24445a28e310c
Esmeralda Nava <ifglitterthen@gmail.com>
2018-12-03 23:07:32 -0600

    dependecias para PWA añadidas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-  apiKey: "AIzaSyBygdRkXhAzqCLuNYdh0r1sksqnQ09oQ-w",
-  authDomain: "finlab-a4ee3.firebaseapp.com",
-  databaseURL: "https://finlab-a4ee3.firebaseio.com",
-  projectId: "finlab-a4ee3",
-  storageBucket: "finlab-a4ee3.appspot.com",
-  messagingSenderId: "939897895686"
+  apiKey: 'AIzaSyBygdRkXhAzqCLuNYdh0r1sksqnQ09oQ-w',
+  authDomain: 'finlab-a4ee3.firebaseapp.com',
+  databaseURL: 'https://finlab-a4ee3.firebaseio.com',
+  projectId: 'finlab-a4ee3',
+  storageBucket: 'finlab-a4ee3.appspot.com',
+  messagingSenderId: '939897895686'
 };
 
 /*
