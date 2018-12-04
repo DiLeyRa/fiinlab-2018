9f253da0f827479fef7cf9168266056946dfbf3f
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-12-03 23:37:15 -0600

    Creando componentes enviar pago

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SubmitComponent } from './components/submit/submit.component';
+import { Submit2Component } from './components/submit2/submit2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'submit', component:SubmitComponent},
+  {path: 'submit2', component:Submit2Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
+  
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubmitComponent } from './components/submit/submit.component';
+import { Submit2Component } from './components/submit2/submit2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubmitComponent,
+    Submit2Component
   ],
   imports: [
     BrowserModule,
