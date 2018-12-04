1c3ee197c7f7f14e659506239667af619342650d
AuraMelisa01 <auramelisa6@gmail.com>
2018-12-03 23:37:24 -0600

    agregando componentes meta o sueño

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DreamComponent } from './components/dream/dream.component';
+import { DreamStatusComponent } from './components/dream-status/dream-status.component';
+import { DreamRegisterComponent } from './components/dream-register/dream-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DreamComponent,
+    DreamStatusComponent,
+    DreamRegisterComponent
   ],
   imports: [
     BrowserModule,
